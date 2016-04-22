862a54ef3f8577cd1c7d03080ec7d1c19aecac6f
Jake Lindvall <jplindvall@comcast.net>
2016-04-22 06:23:33 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+//Intellectual property of Jake Lindvall, 2016
+//Lol steal it I don't care
+
+//package PACKAGE NAME HERE
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Prints the result of the hard-coded function in the "Maths" class
+		//between these two limits of integration.
+		System.out.println(Maths.ridiculousAsciiIntegralPrinter(85, 100));
+
+	}
+
+}
